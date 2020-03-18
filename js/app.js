ade4f0f44c605e19c733bb8fb9838b0048779a1b
Brian Wilson <b@Brians-MacBook-Pro.local>
2020-03-18 11:50:02 -0500

    gets property name and value

--- js/app.js
+++ js/app.js
@@ -1,34 +1,66 @@
 console.log('Guess Who');
 
 // class 
-class Character {
-	constructor(name, gender, facialHair, mustache, beard, hat, glasses, eyeColor, blushing, hairColor, bald, bigNose, earrings) {
-		this.name = name
-		this.gender = gender
-		this.facialHair = facialHair
-		this.mustache = mustache
-		this.beard = beard
-		this.hat = hat
-		this.glasses = glasses
-		this.eyeColor = eyeColor
-		this.blushing = blushing
-		this.hairColor = hairColor 
-		this.bald = bald
-		this.bigNose = bigNose
-		this.earrings = earrings
-	}
-}
+// class Character {
+// 	constructor(name, gender, facialHair, mustache, beard, hat, glasses, eyeColor, blushing, hairColor, bald, bigNose, earrings) {
+// 		this.name = name
+// 		this.gender = gender
+// 		this.facialHair = facialHair
+// 		this.mustache = mustache
+// 		this.beard = beard
+// 		this.hat = hat
+// 		this.glasses = glasses
+// 		this.eyeColor = eyeColor
+// 		this.blushing = blushing
+// 		this.hairColor = hairColor 
+// 		this.bald = bald
+// 		this.bigNose = bigNose
+// 		this.earrings = earrings
+// 	}
+// }
 
 // game app
 const guessWhoApp = {
 
-	characters: [
-	{name: 'Alex', gender: 'male', facialHair: true, mustache: true, beard: false, hat: false, glasses: false, eyeColor: 'brown', blushing: false, hairColor: 'black', bald: false, bigNose: false, earrings: false}, 
-	{name: 'Alfred', gender: 'male', facialHair: true, mustache: true, beard: false, hat: false, glasses: false, eyeColor: 'blue', blushing: false, hairColor: 'orange', bald: false, bigNose: false, earrings: false},
-	{name: 'Anita', gender: 'female', facialHair: false, mustache: false, beard: false, hat: false, glasses: false, eyeColor: 'blue', blushing: true, hairColor: 'yellow', bald: false, bigNose: false, earrings: false}
-	]
-	
+	selectRandomCharacter: function() {
+		const charactersName = characters.name
+		const randCharacter = Math.floor(Math.random() * characters.length)
+	},
+
+	// eliminateCharacter: function(nameArr) {
+	// 	// based on the question type it should eliminate the character 
+	// 	// Question 1 "Is the character a man"
+	//select off name then delete element
+	// 	// if()
+	// }
+
+	//propName = 'gender' , propVal = 'male'
+	selectName: function(propName, propVal) {
+		charNames = []
+		
+		for(let i = 0; i < characters.length; i++) {
+			//console.log(characters[i][propName])
+			if(characters[i][propName] === propVal) {
+				charNames.push(characters[i].name)
+			}
+		}
+		return charNames; 
+	}
 }
+
+//listener on the button 
+//function that takes all characters and checks each property (gender) and then returns the names
+//ex pass in character array
+//returning ['Alex', 'Alfred']
+
+
 
 
 // listeners 
+// const question1 = document.querySelectorAll('#question-1')
+// question1.addEventListener('click', (event) => {
+	//calling select name 
+	//namesToDelete = selectName(); 
+// 	guessWhoApp.eliminateCharacter(namesToDelete);
+
+// })
---
+++ js/characters.js
@@ -0,0 +1,5 @@
+const characters = [
+	{name: 'Alex', gender: 'male', facialHair: true, mustache: true, beard: false, hat: false, glasses: false, eyeColor: 'brown', blushing: false, hairColor: 'black', bald: false, bigNose: false, earrings: false}, 
+	{name: 'Alfred', gender: 'male', facialHair: true, mustache: true, beard: false, hat: false, glasses: false, eyeColor: 'blue', blushing: false, hairColor: 'orange', bald: false, bigNose: false, earrings: false},
+	{name: 'Anita', gender: 'female', facialHair: false, mustache: false, beard: false, hat: false, glasses: false, eyeColor: 'blue', blushing: true, hairColor: 'yellow', bald: false, bigNose: false, earrings: false},
+]
