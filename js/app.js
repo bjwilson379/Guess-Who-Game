b076e8ccba651428bba590936298b2b5668464a3
Brian Wilson <b@Brians-MacBook-Pro.local>
2020-03-19 15:47:54 -0500

    hat question works

--- js/app.js
+++ js/app.js
@@ -18,7 +18,11 @@ const guessWhoApp = {
 
 	},
 
-	compareQuestionAndWinCharacter: function(gender) {
+	startGame: function() {
+
+	},
+
+	compareGenderAndWinCharacter: function(gender) {
 		//loop through characters array
 		for(let i = 0; i < characters.length; i++) {
 			// if win character is a man
@@ -75,12 +79,36 @@ const guessWhoApp = {
 			}
 		}
 	},
-	// eliminateCharacter: function(nameArr) {
-	// 	// based on the question type it should eliminate the character 
-	// 	// Question 1 "Is the character a man"
-	//select off name then delete element
-	// 	// if()
-	// }
+
+	compareHatAndWinCharacter: function() {
+		for(let i = 0; i < characters.length; i++) {
+			if(this.winCharacter.hat === true) {
+
+				if(characters[i].hat === true) {
+
+				} else {
+					let thisPerson = document.querySelector(`#${characters[i].name}`)
+					// console.log("this is thisPerson inside the func\n", thisPerson);
+					thisPerson.classList.add('crossOff')
+				}
+			} else {
+				if(characters[i].hat === false) {
+
+				} else {
+					let thisPerson = document.querySelector(`#${characters[i].name}`)
+					// console.log("this is thisPerson inside the func\n", thisPerson);
+					thisPerson.classList.add('crossOff')	
+				}
+			}
+		}
+	},
+
+	winGame: function() {
+		
+	},
+
+
+
 
 	//propName = 'gender' , propVal = 'male'
 	selectName: function(propName, propVal) {
@@ -97,22 +125,33 @@ const guessWhoApp = {
 }
 
 guessWhoApp.selectRandomCharacter()
-//guessWhoApp.compareQuestionAndWinCharacter()
+//guessWhoApp.compareGenderAndWinCharacter()
 
-
-// Gender Question 1
-const question1 = document.querySelector('#question-1')
-question1.addEventListener('click', (event) => {
-	guessWhoApp.compareQuestionAndWinCharacter(question1);
-})
-
-// Facial Hair Question 2
-const question2 = document.querySelector('#question-2')
-question2.addEventListener('click', (event) => {
-	guessWhoApp.compareFacialHairAndWinCharacter(question2);
+// Start Game
+	const startGameButton = document.querySelector('#start-game')
+	startGameButton.addEventListener('click', (event) => {
+	guessWhoApp.startGame(startGameButton);
 	console.log(event);
 })
 
+// Gender Question - 1
+const question1 = document.querySelector('#question-1')
+question1.addEventListener('click', (event) => {
+	guessWhoApp.compareGenderAndWinCharacter(question1);
+})
+
+// Facial Hair Question - 2
+const question2 = document.querySelector('#question-2')
+question2.addEventListener('click', (event) => {
+	guessWhoApp.compareFacialHairAndWinCharacter(question2);
+})
+
+// Facial Hair Question - 2
+const question3 = document.querySelector('#question-3')
+question3.addEventListener('click', (event) => {
+	guessWhoApp.compareHatAndWinCharacter(question3);
+	console.log(event);
+})
 
 console.log("this is the winCharacter\n", guessWhoApp.winCharacter)
 
